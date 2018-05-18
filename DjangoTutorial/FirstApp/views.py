1094e93b539159de25a441220bc47e9800a82d3d
KodyKendall <kodyckendall@gmail.com>
2018-05-18 06:12:23 -0600

    Added in/changed the views.py to serve our index.html file (located at templates/FirstApp/index.html), as well as adding in a .css stylesheet in our static folder.

--- DjangoTutorial/FirstApp/views.py
+++ DjangoTutorial/FirstApp/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def index(request):
-	return HttpResponse("Hello, Django!")
+	now = datetime.now()
+
+	return render(
+		request,
+		"FirstApp/index.html", #Relative path from the 'templates' folder to the template file.
+		{
+			'title' : "First App!",
+			'content' : "Hello Django! on " + now.strftime("%A, %d, %B, %Y, at %X"),
+			'message' : "Welcome to your first Django App"
+		}
+	)
